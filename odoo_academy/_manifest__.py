711d7b55adb23b4fa782838d4d7680a5475bfe6b
hezekiahes <walter.mcclure@captivea.us>
2022-04-24 17:44:53 -0400

    creation of Odoo Academy module

---
+++ odoo_academy/_manifest__.py
@@ -0,0 +1,26 @@
+# .*. coding: utf-8 .*.
+
+{
+    'name':'Odoo Academy',
+    'summary': """Academy app to manager Training""",
+    'description' : """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'author': 'Odoo',
+    'website': 'https://odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
