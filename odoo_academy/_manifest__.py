db515d3893bceb48c2fdb27e55695da8636a9536
hezekiahes <walter.mcclure@captivea.us>
2022-04-24 17:58:22 -0400

    Update _manifest__.py

--- odoo_academy/_manifest__.py
+++ odoo_academy/_manifest__.py
@@ -1,26 +1,21 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name':'Odoo Academy',
-    'summary': """Academy app to manager Training""",
-    'description' : """
-        Academy Module to manage Training:
-        - Courses
-        - Sessions
-        - Attendees
-        """,
-    'author': 'Odoo',
-    'website': 'https://odoo.com',
-    'category': 'Training',
-    'version': '0.1',
+{
+    'name': "Odoo Academy",
+    'version': '1.0',
     'depends': ['base'],
+    'author': "Walter McClure",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
     'data': [
-        
-        
+        'views/mymodule_view.xml',
     ],
-    
+    # data files containing optionally loaded demonstration data
     'demo': [
-        
-        
+        'demo/demo_data.xml',
     ],
 }
