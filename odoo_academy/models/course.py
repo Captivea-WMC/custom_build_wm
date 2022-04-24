373b40d7a83340303c1d22cdb1493a25830a4926
hezekiahes <walter.mcclure@captivea.us>
2022-04-24 19:22:16 -0400

    removed level fields

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -9,14 +9,7 @@ class Course(models.Model):
     
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
-    
-    level = fields.Selection(string='Level',
-                                selection=[('beginner', 'Beginner'),
-                                           ('intermediate', 'Intermediate),
-                                           ('advanced', 'Advanced')],
-                                copy=False)
-                                
-                                
+
     active = fields.Boolean(string='Active', default=True)
                                 
 
