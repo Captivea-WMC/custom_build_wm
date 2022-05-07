6592764ea23e8d89ead228b7ecad68afd827d7b4
hezekiahes <walter.mcclure@captivea.us>
2022-05-07 16:37:40 -0400

    Update course.py

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -8,7 +8,9 @@ class Course(models.Model):
   _name = 'academy.course'
   _description = 'Course Info'
 
+
 name = fields.Char(string='Title', required=True)
+
 description = fields.Text(string='Description')
 
 level = fields.Selection(string='Level',
